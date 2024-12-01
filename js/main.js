eb2a68de746a5c9865e528ac7befa3531f1c7b9d
Davon Morris <rootbeer587@gmail.com>
2024-12-01 13:23:53 -0600

    2 and 3 Done.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//* #1
 function createElemWithText (element = "p", textContent = "", className){
     let createdElement = document.createElement(element);
     createdElement.textContent = textContent;
@@ -6,3 +7,32 @@ function createElemWithText (element = "p", textContent = "", className){
     }
     return createdElement;
 }
+//* #2
+const createSelectOptions = (users) => {
+    if(!users){
+        return undefined;
+    }
+
+    const optionsArray = [];
+
+    for(const user of users){
+        const newOption = document.createElement("option");
+
+        newOption.value = user.id
+        newOption.textContent = user.name
+        optionsArray.push(newOption);
+    }
+    return optionsArray;
+}
+//* #3
+const toggleCommentSection = (postID) => {
+    if(!postID){
+        return undefined;
+    }
+    const section = document.querySelector(`[data-post-id="${postID}"]`);
+    if(section){
+       section.classList.toggle("hide")
+    }
+    return section;
+}
+//* #4
